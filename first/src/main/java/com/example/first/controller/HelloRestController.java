baa1cdae353507e19beae80fe2d7f8475137dabb
Khushdilbaliyan11 <136252776+Khushdilbaliyan11@users.noreply.github.com>
2025-03-02 00:03:37 +0530

    Add files via upload

---
+++ first/src/main/java/com/example/first/controller/HelloRestController.java
@@ -0,0 +1,45 @@
+package com.example.first.controller;
+
+
+import org.springframework.web.bind.annotation .*;
+import com.example.first.model.User;
+
+@RestController
+
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    // curl localhost:8080/hello -w "\n"
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From BridgeLabz! ! !";
+
+    }
+
+    // curl localhost:8080/hello/query?name=Naxayan -w "\n"
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "1";
+    }
+
+    // curl localhost:8080/hello/param/Naxayan -w "\n"
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    // curl -X POST -H "Content-Type: application/ison"
+    //               -d '{"firstName": "Lisa", "lastName": "Harry"}'
+    //   "http://localhost: 8080/hello/post" -w "\n"
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + "  " + user.getLastName() + "!";
+    }
+
+    // curl -X PUT localhost: 8080/hello/put/Lisa/?lastName=Terrisa -w "\n"
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}

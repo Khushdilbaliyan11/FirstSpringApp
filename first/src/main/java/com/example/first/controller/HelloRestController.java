2dd3f21dff8a16db6d49572f8bcc50fc3b197f61
Khushdilbaliyan11 <136252776+Khushdilbaliyan11@users.noreply.github.com>
2025-03-02 00:26:21 +0530

    Add files via upload

--- first/src/main/java/com/example/first/controller/HelloRestController.java
+++ first/src/main/java/com/example/first/controller/HelloRestController.java
@@ -22,24 +22,6 @@ public class HelloRestController {
         return "Hello " + name + "1";
     }
 
-    // curl localhost:8080/hello/param/Naxayan -w "\n"
-    @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
 
-    // curl -X POST -H "Content-Type: application/ison"
-    //               -d '{"firstName": "Lisa", "lastName": "Harry"}'
-    //   "http://localhost: 8080/hello/post" -w "\n"
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user) {
-        return "Hello " + user.getFirstName() + "  " + user.getLastName() + "!";
-    }
 
-    // curl -X PUT localhost: 8080/hello/put/Lisa/?lastName=Terrisa -w "\n"
-    @PutMapping("/put/{firstName}")
-    public String sayHello(@PathVariable String firstName,
-                           @RequestParam(value = "lastName") String lastName) {
-        return "Hello " + firstName + " " + lastName + "!";
-    }
 }
